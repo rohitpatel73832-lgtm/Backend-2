fbe13ec9eb9ffbf500f4cdd5081279939ccfda4d
rohitpatel73832-lgtm <rohitpatel73832@gmail.com>
2026-03-18 23:20:58 +0530

    learn how to take user data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //require('dotenv').config({path: './env'})
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-
+import express from "express";
 import dotenv from "dotenv";
+
+
 
 
 import connectDB from "./db/index.js";
@@ -11,8 +13,16 @@ dotenv.config({
     path: './env'
 })
 
-
+ const app = express();
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed!!!",err);
+})
 
 
 
